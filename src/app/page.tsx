9ae6e6bb05d0dfe0b49c198f860d2192d66a82e2
agacia1805 <agata.dopierala@vaimo.com>
2023-11-11 12:37:55 +0000

    feat(api): set up react query and display a list of articles from 3 different APIs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import {Footer } from './slices/Footer';
 import {Header } from './slices/Header';
-
+import {Articles} from './Articles';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col justify-between">
     <Header />
+    <Articles />
     <Footer />
     </main>
   )
