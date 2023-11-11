3a323dae203df0e3183776ab6bfa1c8c199c39bc
agacia1805 <agata.dopierala@vaimo.com>
2023-11-11 19:51:47 +0000

    feat(filter): implement filter by general functions for filtering by author, keyword, source, category and date

--- src/app/Articles.tsx
+++ src/app/Articles.tsx
@@ -1,47 +1,130 @@
 'use client';
 
 import * as React from "react";
+import {useMemo} from 'react';
 import { useQuery } from "@tanstack/react-query";
+import { Fragment, useState } from 'react'
+import { Dialog, Disclosure, Transition } from '@headlessui/react'
+import { XMarkIcon } from '@heroicons/react/24/outline'
+import { ChevronDownIcon, PlusIcon } from '@heroicons/react/20/solid'
+import { useHistory, useLocation } from 'react-router-dom';
+
 
 export const Articles = () => {
 const newsApiKey = '66e91b5815f04ed792631df8cdfc0822';
 const guardianApiKey = '84411a34-7972-4451-b787-dca38e5ea6dc';
 const nytApiKey = '5WttEG3a28sAs6NX0i5X23WK3ZXAQ4Rj';
+const param = '';
 
-const {data: newsApiData} = useQuery({queryKey: ['newsApiArticles'], queryFn: async () => {
-   const response = await fetch(`https://newsapi.org/v2/everything?q=bitcoin&apiKey=${newsApiKey}`);
-   const data = await response.json();
-   return data;
-}});
+const filters = [
+  {
+    id: 'color',
+    name: 'Color',
+    options: [
+      { value: 'purple', label: 'Purple' },
+    ],
+  },
+  {
+    id: 'category',
+    name: 'Category',
+    options: [
+      { value: 'pants-shorts', label: 'Pants & Shorts' },
+    ],
+  },
+  {
+    id: 'sizes',
+    name: 'Sizes',
+    options: [
+      { value: '2xl', label: '2XL' },
+    ],
+  },
+]
 
-const {data: guardianApiData} = useQuery({queryKey: ['guardianApiArticles'], queryFn: async () => {
-   const response = await fetch(`https://content.guardianapis.com/search?api-key=${guardianApiKey}`);
-   const data = await response.json();
-   return data;
-}});
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+  const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false)
 
-const {data: nytApiData} = useQuery({queryKey: ['nytApiArticles'], queryFn: async () => {
-   const response = await fetch(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${nytApiKey}`);
-   const data = await response.json();
-   return data;
-}});
+// const filterByAuthorGeneral = (data, author) => {
+//   return [].concat(...data)?.filter(article => {
+//       const articleAuthor = article?.author || article?.byline || (article?.tags?.map(tag => tag.webTitle).join(', '));
+//       return articleAuthor?.includes(author);
+//     });
+// };
+// // console.log(filterByAuthorGeneral([nytApiData, newsApiData, guardianApiData], 'do'));
+//
+// const filterByKeywordGeneral = (data, keyword) => {
+//   return [].concat(...data)?.filter(article => {
+//       const articleKeyword = article?.adx_keywords || article?.webTitle || article?.title;
+//       return articleKeyword?.includes(keyword);
+//     });
+// };
+// // console.log(filterByKeywordGeneral([nytApiData, newsApiData, guardianApiData], 'Israel'));
+//
+// const filterBySourceGeneral = (data, source) => {
+//   return [].concat(...data)?.filter(article => {
+//       const articleSource =  article?.source?.name || article?.source || (article?.webUrl?.includes('theguardian') ? 'The Guardian' : null);
+//       return articleSource?.includes(source);
+//     });
+// };
+//
+// // console.log(filterBySourceGeneral([nytApiData, newsApiData, guardianApiData], 'Gizmo'));
+//
+// const filterByCategoryGeneral = (data, category) => {
+//   return [].concat(...data)?.filter(article => {
+//       const articleCategory = article?.section || article?.pillarName;
+//       return articleCategory?.includes(category);
+//     });
+// };
+//
+// // console.log(filterByCategoryGeneral([nytApiData, newsApiData, guardianApiData], 'Sport'));
+//
+// const filterByDateGeneral = (data, category) => {
+//   return [].concat(...data)?.filter(article => {
+//       const articleCategory = article?.section || article?.pillarName;
+//       return articleCategory?.includes(category);
+//     });
+// };
 
-console.log('newsApiData', newsApiData);
-console.log('guardianApiData', guardianApiData);
-console.log('nytApiData', nytApiData);
+// const filterDates = (dataArray, thresholdDate) => {
+//   const thresholdDateObj = new Date(thresholdDate);
+//   const filteredArrays = dataArray.map(data => {
+//     return data.filter(article => {
+//       const dateField = Object.keys(article).find(fieldName => {
+//         const dateString = article[fieldName];
+//         return dateString && new Date(dateString).toString() !== 'Invalid Date';
+//       });
+//
+//       if (!dateField) {
+//         return false;
+//       }
+//
+//       const articleDate = new Date(article[dateField]);
+//
+//       return !isNaN(articleDate) && articleDate > thresholdDateObj;
+//     });
+//   });
+//
+//   return [].concat(...filteredArrays);
+// };
+//
+// console.log(filterDates([nytApiData, newsApiData, guardianApiData], '2023-10'));
+
+const fetchArticlesFromApi = async (apiKey, endpoint) => {
+        const response = await fetch(`${endpoint}${apiKey}`);
+        const data = await response.json();
+        return data;
+ };
+
+const { data: newsApiData, isLoading: isLoadingNewsApi } = useQuery({queryKey: ['newsApiArticles'],
+    queryFn: () => fetchArticlesFromApi(newsApiKey, 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=')});
+const { data: guardianApiData, isLoading: isLoadingGuardian } = useQuery({queryKey: ['guardianApiArticles'],
+    queryFn: () => fetchArticlesFromApi(guardianApiKey, 'https://content.guardianapis.com/search?&show-tags=contributor&api-key=')});
+const { data: nytApiData, isLoading: isLoadingNytApi } = useQuery({queryKey: ['nytApiArticles'],
+    queryFn: () => fetchArticlesFromApi(nytApiKey, 'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=')});
 
   return (
-    <div>
-        {newsApiData?.articles.map((item) => {
-            return <p>{item.title}</p>
-        })}
-        {guardianApiData?.response.results.map((item) => {
-                    return <p>{item.webTitle}</p>
-                })}
-
-        {nytApiData?.results.map((item) => {
-                    return <p>{item.title}</p>
-                })}
-    </div>
-  );
+    <div>Test</div>
+      );
 };
+
