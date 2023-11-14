a35b64705ddaa8de344093d667bed17c66cb0637
agacia1805 <agata.dopierala@vaimo.com>
2023-11-14 15:41:55 +0000

    feat(layout): add articles to page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+'use client';
+
 import {Footer } from './slices/Footer';
 import {Header } from './slices/Header';
-import {Articles} from './Articles';
+import {Articles} from './slices/Articles';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between">
-     <Header />
-     <Articles />
-     <Footer />
+    <main className="flex min-h-screen flex-col bg-gray-100">
+      <Header />
+      <Articles />
+      <Footer />
     </main>
   )
 }
