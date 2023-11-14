d7d932c0aa78a619d0eb7b519d24965b084598c6
agacia1805 <agata.dopierala@vaimo.com>
2023-11-14 15:39:08 +0000

    chore(header): increase font size for header

--- src/app/slices/header/Header.tsx
+++ src/app/slices/header/Header.tsx
@@ -7,8 +7,8 @@ export const Header = () => {
           <header className="bg-gray-100 shadow">
             <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:divide-y lg:divide-gray-200 lg:px-8">
               <div className="relative flex h-16 justify-between">
-                <div className="relative z-10 flex px-2 lg:px-0">
-                  <div className="flex flex-shrink-0 items-center text-xl font-bold text-indigo-600">
+                <div className="relative z-10 flex px-2 lg:px-0 ">
+                  <div className="flex flex-shrink-0 items-center text-3xl font-bold text-indigo-600 ">
                     <Link href="/">NewsByte</Link>
                   </div>
                 </div>
